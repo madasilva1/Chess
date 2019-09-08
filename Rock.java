7b81edf1df5d483f35f14977c598927384334949
marcos <madasilva195@gmail.com>
2019-09-07 22:38:06 -0400

    Object position replaced by Getposition

---
+++ Rock.java
@@ -0,0 +1,17 @@
+public class Rock extends Piece{
+    Position position;
+       Rock(int cl,int rw) {
+          //int myRock = position.GetColum();
+           this.position.GetColum();//GetColum has replaced colum becouse a made colum private in Pisition class
+                                   // now a had to use GetClum method to acess it, samething in GetRow
+           this.position.GetRow();
+       }
+    boolean isValidMove(Position newPosition) {
+        if (newPosition.GetColum() == this.position.GetColum() || newPosition.GetRow() == this.position.GetRow() ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
