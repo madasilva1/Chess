5e6a0e253b9c9e073f2fda64879fe6e31f36f5f7
marcos <madasilva195@gmail.com>
2019-09-07 22:23:57 -0400

    Test position to mahe surre queen is on boudary

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+        Qeen queen = new Qeen();
+        Position testPosition = new Position(3,10);
+        if(queen.isValidMove(testPosition)){
+            System.out.println("Yes, I can move there.");
+        }
+        else{
+            System.out.println("Nope, can't do!");
+        }
+
+    }
+}
