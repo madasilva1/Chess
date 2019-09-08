3673138271aaff4bdc7105196ce9dce905aa2937
marcos <madasilva195@gmail.com>
2019-09-07 22:28:02 -0400

    row and column set privated Gets Sets criated

---
+++ Position.java
@@ -0,0 +1,25 @@
+public class Position {
+  private   int row;
+  private   int colum;
+
+    Position(int row, int colum){
+        this.colum = colum;
+        this.row = row;
+    }
+    public void SetRowColum(int rw){
+          row = rw;
+    }
+    public void SetColum(int cl) {
+        colum = cl;
+    }
+    public int GetRow(){
+        return  row;
+    }
+    public int GetColum(){
+
+        return  colum;
+    }
+
+
+
+}
