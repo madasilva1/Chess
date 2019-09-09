212aeda340b17df1250dca2a61a767f22f0b6866
Marcos <madasilva195@gmail.com>
2019-09-09 15:52:47 -0400

    Udate Queen

--- Qeen.java
+++ Qeen.java
@@ -1,3 +1,11 @@
 public class Qeen extends Piece {
+    Queen queen = new Queen(3,9);
+        Position testPosition = new Position(3,9);
+        if(queen.isValidMove(testPosition)){
+            System.out.println("Yes, I can move there.");
+        }
+        else{
+            System.out.println("Nope, can't do!");
+        }
 
 }
