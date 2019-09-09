40d575d68f369000c45640b1c47b4bf416d6227f
marcos <madasilva195@gmail.com>
2019-09-09 19:48:51 -0400

    Queen update

--- Qeen.java
+++ Qeen.java
@@ -1,12 +1,6 @@
 public class Qeen extends Piece {
-        //Piece pice = new Piece();
-    Qeen queen = new Queen(3,9);
-        Position testPosition = new Position(3,9);
-        if(queen.G(testPosition)){
-            System.out.println("Yes, I can move there.");
-        }
-        else{
-            System.out.println("Nope, can't do!");
-        }
+
+
+
 
 }
