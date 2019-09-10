369731efc4ffbabc36ca1b372432dea5085b8000
marcos <madasilva195@gmail.com>
2019-09-09 20:38:58 -0400

    Rock update

--- Rock.java
+++ Rock.java
@@ -1,8 +1,8 @@
-
 public class Rock extends Piece{
     Position position;
+
        Rock(int cl,int rw) {
-          //int myRock = position.GetColum();
+
            this.position.GetColum();//GetColum has replaced colum becouse a made colum private in Pisition class
                                    // now a had to use GetClum method to acess it, samething in GetRow
            this.position.GetRow();
