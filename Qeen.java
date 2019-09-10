af076b6fd5d8210e26b7420a59ce0b22b8cea30c
marcos <madasilva195@gmail.com>
2019-09-09 20:24:36 -0400

    Queen has been finaly fixed(but not totaly)

--- Qeen.java
+++ Qeen.java
@@ -1,6 +1,18 @@
 public class Qeen extends Piece {
+    Position position;
+
+    Qeen(int cl, int rw) {
+        this.position.GetColum();
+        this.position.GetColum();
+    }
+
+    boolean isValidMove(Position newPosition) {
+        if (newPosition.GetColum() == this.position.GetColum() || newPosition.GetRow() == this.position.GetRow() && Math.abs(newPosition.GetColum() - this.position.GetColum()) == Math.abs(newPosition.GetRow() - this.position.GetRow())) {
+            return true;
+        } else {
+            return false;
+        }
 
 
-
-
+    }
 }
