f81ff0c9faaf2c48caa912e77cf1a00da01f128f
marcos <madasilva195@gmail.com>
2019-09-09 20:38:18 -0400

    Main update

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@
 public class Main {
     public static void main(String[] args){
 
-        Qeen queen = new Qeen();
+        Rock rock = new Rock(3,9);
         Position testPosition = new Position(3,10);
-        if(queen.isValidMove(testPosition)){
+        if(rock.isValidMove(testPosition)){
             System.out.println("Yes, I can move there.");
         }
         else{
